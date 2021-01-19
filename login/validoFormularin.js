c0b80a3c9f41d587e7ecba7f670044d38d80ecc6
Labinot Krasniqi <lk49275@ubt-uni.net>
2021-01-19 23:18:19 +0100

    commitbyLabinot

---
+++ login/validoFormularin.js
@@ -0,0 +1,34 @@
+﻿document.addEventListener("DOMContentLoaded",
+
+function(ngjarja) {
+
+const BtnSubmit = document.getElementById('Btn-submit');
+
+/*Funksioni per te e validuar fushat e formes */
+const validate = (ngjarja) => {
+
+const perdoruesi = document.getElementById('userid');
+const fjalkalimi = document.getElementById('pass');
+
+
+if (perdoruesi.value === "") {
+alert("Ju lutem shtoni perdoruesin.");
+perdoruesi.focus();
+return false;
+}
+if (fjalkalimi.value === "") {
+alert("Ju lutem shtoni Fjalkalimin.");
+fjalkalimi.focus();
+return false;
+}
+
+
+return true; 
+}
+
+
+
+
+
+BtnSubmit.addEventListener('click', validate);
+});
