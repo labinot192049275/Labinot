6d688adb98c31e7956d20490c9f631be5c1b3083
Labinot Krasniqi <lk49275@ubt-uni.net>
2021-02-13 21:18:42 +0100

    CommitByLabi

---
+++ Products/headerAndProducts.php
@@ -0,0 +1,50 @@
+﻿
+<!Doctype html>
+
+<head>  </head>
+
+<link rel="stylesheet" href="style.css">  
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css">
+
+<body>
+
+<section class="container-banner">
+<header class="header">
+    <div class="logo">
+        <a href="">Auto Salloni <span>Prishtina</span></a>
+    </div>
+    <div class="content-nav">
+        <nav class="nav">
+        <ul>
+            <li><a href="../index.php">HOME</a></li>
+            <li><a href="../login/login -L.K.html">KYQU</a></li>
+             <li><a href="../Regjistrimi-html,Css/Regjistrimi.html ">REGJISTRIMI</a></li>
+             <li><a href="../About us/Aboutus.html">INFO</a></li>
+        </ul>
+    </nav>
+   <div class="search-icon">
+    <input class="txt" type="search" name="" id="" placeholder="kërkim">
+   <i class="fas fa-search"></i><input class="btnbuscar" type="submit" value="">  <!-- ikona search-->
+      
+   </div>
+</div>
+
+</header>
+
+
+</body>
+
+
+</html>
+
+
+
+<?php
+
+include './Products.php';
+
+?>
+
+
+
+
